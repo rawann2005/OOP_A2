ae4f3aced79df3fe7960507f450675ec5f1010bb
Malak <malakraouf2006@gmail.com>
2025-11-03 20:46:56 +0200

    malak

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -4,9 +4,17 @@ MainComponent::MainComponent()
 {
 
     addAndMakeVisible(player1);
+    addAndMakeVisible(addPlayerButton);
 
-    setSize(1000, 500);
+    addPlayerButton.addListener(this);
+    addPlayerButton.setColour(juce::TextButton::buttonColourId, juce::Colours::darkgrey);
+    addPlayerButton.setColour(juce::TextButton::textColourOffId, juce::Colours::white);
+
+
+    setSize(800, 500);
     setAudioChannels(0, 2);
+
+
 }
 
 MainComponent::~MainComponent()
@@ -16,23 +24,53 @@ MainComponent::~MainComponent()
 
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
-    player1.prepareToPlay(samplesPerBlockExpected, sampleRate);
+
+    mixerSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+
+
+    mixerSource.addInputSource(player1.getAudioSource(), false);
+    mixerSource.addInputSource(player2.getAudioSource(), false);
 }
 
 void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
 {
 
-    player1.getNextAudioBlock(bufferToFill);
-
+    mixerSource.getNextAudioBlock(bufferToFill);
 }
 
 void MainComponent::releaseResources()
 {
-    player1.releaseResources();
+    mixerSource.releaseResources();
 }
 
+void MainComponent::buttonClicked(juce::Button* button)
+{
+
+    if (button == &addPlayerButton)
+    {
+        showSecondPlayer = true;
+        addAndMakeVisible(player2);
+        addPlayerButton.setVisible(false);
+        resized();
+    }
+}
 
 void MainComponent::resized()
 {
-    player1.setBounds(20, 20, getWidth() - 40, 300); 
+
+    auto area = getLocalBounds();
+
+    if (showSecondPlayer)
+    {
+
+        player1.setBounds(area.removeFromLeft(getWidth() / 2));
+        player2.setBounds(area);
+    }
+    else
+    {
+
+        auto buttonArea = area.removeFromBottom(60);
+        player1.setBounds(area);
+        addPlayerButton.setBounds(buttonArea.withSizeKeepingCentre(200, 40));
+    }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,7 +2,8 @@
 #include <JuceHeader.h>
 #include "PlayerGUI.h"
 
-class MainComponent : public juce::AudioAppComponent
+class MainComponent : public juce::AudioAppComponent,
+    public juce::Button::Listener
 {
 public:
     MainComponent();
@@ -12,10 +13,19 @@ public:
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
     void resized() override;
-
-
+    void buttonClicked(juce::Button* button) override;
 
 private:
+
     PlayerGUI player1;
+    PlayerGUI player2;
+
+
+    juce::TextButton addPlayerButton{ "Add Second Player" };
+    bool showSecondPlayer = false;
+
+
+    juce::MixerAudioSource mixerSource;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
