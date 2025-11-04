1a37e027ba753f138a9e0cd720e8859f360a75b5
jomanafarid13 <jomanafarid210@gmail.com>
2025-11-04 14:00:38 +0200

    Update PlayerGUI.h

--- Source/PlayerGUI.h
+++ Source/PlayerGUI.h
@@ -5,7 +5,8 @@
 class PlayerGUI : public juce::Component,
     public juce::Button::Listener,
     public juce::Slider::Listener,
-    public juce::Timer
+    public juce::Timer,
+    public juce::ListBoxModel
 {
 public:
     PlayerGUI();
@@ -20,6 +21,7 @@ public:
     void timerCallback() override;
 
     juce::AudioTransportSource* getAudioSource();
+
 private:
     PlayerAudio playerAudio;
 
@@ -48,10 +50,28 @@ private:
     bool isMuted = false;
     float lastVolume = 0.5f;
 
+    // Metadata
+    juce::Label infolabel;
+
+    // Playlist
+    juce::ListBox playlistBox{"PlayList", this};
+    std::vector<juce::File> playlist;
+  
+    //void loadTrack(const juce::File& file);
     std::unique_ptr<juce::FileChooser> fileChooser;
 
+    // ListBoxModel
+    int getNumRows() override;
+    void paintListBoxItem(int rowNumber, juce::Graphics& g, int width, int height, bool rowIsSelected) override;
+    void selectedRowsChanged(int lastRowSelected) override;
+
+    // Event handlers
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
+
+    // Helpers
+    void loadMultipleFiles();
+    void updateInfoLabel(const juce::File& file);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
 };
